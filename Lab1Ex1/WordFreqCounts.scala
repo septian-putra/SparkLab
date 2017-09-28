546b65af8695d11e80863c4cce2447d418b5c847
Ubuntu@AWS <ubuntu@ec2-54-186-13-60.us-west-2.compute.amazonaws.com>
2017-09-28 09:22:46 +0000

    Finished the code and tested with the big.txt

--- Lab1Ex1/WordFreqCounts.scala
+++ Lab1Ex1/WordFreqCounts.scala
@@ -29,35 +29,41 @@ object WordFreqCounts
         // Cache the data in memory for faster, repeated retrieval.
         wordFreq.cache
         
+        
+        
+        
         val outRDD = wordFreq
-        // Convert each line to lower case
-        .map(line => line.toLowerCase)
-        // Insert stop token () for sliding
-        .map(word => ("# "+word))
-        // Split each line on the spaces and then use sliding to get succesive pair of words
-        .flatMap(row => row.split(' ').sliding(2).toList)
+        // Insert stop token (#) for sliding and convert each line to lower case
+        .map(row => ("# "+row).toLowerCase)
+        // Split 2 words connected with "--" by adding stop token (#) after ("--")  
+        .flatMap{case word => word.replaceAll("--","--# ").split("--")}
+        // Split each line on the spaces, filter the null string and then use sliding to get succesive pair of words
+        .flatMap(row => row.split(' ').filter(_.size>0).sliding(2).toList)
+        // Filter the array to make sure it contains string pair
+        .filter{_.size == 2}
         // Convert word pair into tuples with the second word as key. If the key contain any punctuation, add token in the end of their precedent word
-        .map{case Array(b, a) => if (!Character.isLetter(a.head)) (a.replaceAll("""[\p{Punct}&&[^-]]""", ""), b + "#") else (a.replaceAll("""[\p{Punct}&&[^-]]""", ""), b)}
+        .map{case Array(b, a) => if (Character.isLetter(a.head) && (b.size > 0)) (a.replaceAll("""[\p{Punct}&&[^-]]""", ""), b) else (a.drop(1).replaceAll("""[\p{Punct}&&[^-]]""", ""), b+ "#")}
         // Clean any punctuation from the start of precedent word, but replace with token(#) if it is in the end 
-        .map{case (x, y) => if (!Character.isLetter(y.last)) (x, y.replaceAll("""[\p{Punct}&&[^-]]""", "") + "#") else (x, y.replaceAll("""[\p{Punct}&&[^-]]""", ""))}
+        .map{case (x, y) => if (!Character.isLetter(y.last)) (x, y.dropRight(1).replaceAll("""[\p{Punct}&&[^-]]""", "") + "#") else (x, y.replaceAll("""[\p{Punct}&&[^-]]""", ""))}
         // Group by the key 
         .groupBy( _._1 ).mapValues( _.map( _._2 ))
         // Count the key and all of its precedent
         .map{x => (x._1, x._2.size, x._2.groupBy(identity).mapValues(_.size).toList)}
-        // Sort the element based on the counter
-        .map{x => (x._1, x._2, x._3.sortWith(_._2 > _._2))}.sortBy(_._2,false)
+        // Filter the preceding word and then sort them based on the counter and alphabet
+        .map{x => (x._1, x._2, x._3.filter{_._1.last.isLetter}.sortWith(_._1 < _._1).sortWith(_._2 > _._2))}
+        // Sort the key words based on the counter and alphabet
+        .sortBy(_._1).sortBy(_._2,false)
         // Collect the value
         .collect
         
         //Print to file
         val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("outFile.txt")))
-        for (x <- outRDD1) {
-          writer.write(x._1 + ":"+ x._2)
-          for (y <- x._3){
-            if (Character.isLetter(y._1.last))
+        for (x <- outRDD) {
+            writer.write(x._1 + ":"+ x._2)
+            for (y <- x._3){
                 writer.write("\r\n\t"+ y._1 + ":"+ y._2)
-          }
-          writer.write("\r\n")
+            }
+            writer.write("\r\n")
         }
         writer.close()
         
